71f6feaa0c57ac91e0bcf0d1b4d0d3988a40bf78
Iskren201 <iskren201@gmail.com>
2023-11-02 09:03:13 +0200

    Experience

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import SocialLinks from './components/SocialLinks/SocialLinks';
 import About from './components/About/About';
 import { Portfolio } from './components/Portfolio/Portfolio';
+import { Experience } from './components/Experience/Experience';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Home />
             <About />
             <Portfolio />
+            <Experience />
 
             <SocialLinks />
         </div>
